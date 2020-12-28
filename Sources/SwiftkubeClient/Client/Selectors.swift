ef1a3126c3e540f8222a5bafb6c12e4de20919c0
Iskandar Abudiab <mail@iabudiab.dev>
2020-12-28 15:52:18 +0100

    Change personal copyright to Swiftkube Project

--- Sources/SwiftkubeClient/Client/Selectors.swift
+++ Sources/SwiftkubeClient/Client/Selectors.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2020 Iskandar Abudiab (iabudiab.dev)
+// Copyright 2020 Swiftkube Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
