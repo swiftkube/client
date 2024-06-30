90f9fdf61c369da525fb1bd68a6bc3d5ac7009f8
Iskandar Abudiab <mail@iabudiab.dev>
2024-07-01 00:58:35 +0200

    Update dependencies and raise minimum Swift version to 5.8

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,12 +15,12 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/swiftkube/model.git", .upToNextMinor(from: "0.13.0")),
-		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.19.0")),
-		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.5.3")),
-		.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.4.1")),
-		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.0.6")),
-		.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.61.0")),
+		.package(url: "https://github.com/swiftkube/model.git", .upToNextMinor(from: "0.14.0")),
+		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.21.2")),
+		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.6.1")),
+		.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.5.0")),
+		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.1.2")),
+		.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.67.0")),
 	],
 	targets: [
 		.target(
