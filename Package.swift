17cd4e601fcf744b93e55159ea4d79ac2ba094cf
Iskandar Abudiab <mail@iabudiab.dev>
2025-03-02 14:50:17 +0100

    Sort packages in Package.swift

--- Package.swift
+++ Package.swift
@@ -15,24 +15,24 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/swiftkube/model.git", .upToNextMinor(from: "0.16.0")),
-		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.24.2")),
 		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.6.2")),
 		.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.5.1")),
-		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.1.3")),
 		.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.80.0")),
+		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.1.3")),
+		.package(url: "https://github.com/swiftkube/model.git", .upToNextMinor(from: "0.16.0")),
+		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.24.2")),
 	],
 	targets: [
 		.target(
 			name: "SwiftkubeClient",
 			dependencies: [
-				.product(name: "SwiftkubeModel", package: "model"),
 				.product(name: "AsyncHTTPClient", package: "async-http-client"),
 				.product(name: "Logging", package: "swift-log"),
 				.product(name: "Metrics", package: "swift-metrics"),
-				.product(name: "Yams", package: "Yams"),
 				.product(name: "NIO", package: "swift-nio"),
 				.product(name: "NIOFoundationCompat", package: "swift-nio"),
+				.product(name: "SwiftkubeModel", package: "model"),
+				.product(name: "Yams", package: "Yams"),
 			]
 		),
 		.testTarget(
