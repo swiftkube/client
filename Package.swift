19672133fcd359f52fb7261965e9b6cc8038f572
Iskandar Abudiab <mail@iabudiab.dev>
2026-03-29 22:57:41 +0200

    Update dependencies
    Newer dependency versions dropped Swift 5.10, consequently, this will be
    the last version of SwiftkubeClient supporting Swift 5.10

--- Package.swift
+++ Package.swift
@@ -20,12 +20,12 @@ let package = Package(
 		),
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.6.2")),
-		.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.5.1")),
-		.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.80.0")),
-		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.1.3")),
-		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.24.2")),
+		.package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.6.4")),
+		.package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.7.1")),
+		.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.86.2")),
+		.package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "5.4.0")),
 		.package(url: "https://github.com/swiftkube/model.git", .upToNextMinor(from: "0.19.0")),
+		.package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMajor(from: "1.29.1")),
 	],
 	targets: [
 		.target(
